f87329db968cc8fa2d13c4e2c68674a8b52978a1
k3n <racelisjk@gmail.com>
2023-02-27 05:03:38 +0800

    added logout functionality

--- js/dashboard_firstRender.js
+++ js/dashboard_firstRender.js
@@ -55,6 +55,8 @@ const btnDashboard1 = document.querySelector("#btn-dashboard1");
 const btnDashboard2 = document.querySelector("#btn-dashboard2");
 const container = document.querySelector("#container");
 const tittlePage = document.querySelector("#tittlePage");
+const logoutBtn1 = document.querySelector("#logoutBtn1");
+const logoutBtn2 = document.querySelector("#logoutBtn2");
 
 // temp storage
 const accountData = JSON.parse(localStorage.getItem("tempAccountData"));
@@ -75,10 +77,21 @@ const currentAccount = () => {
   if (sessionAccount === null) {
     container.innerHTML = pettyHuman();
 
+    btnDashboard2.classList.remove("btn-primary");
+    btnDashboard2.classList.remove("text-white");
+    btnDashboard2.classList.add("btn-outline-light");
+    btnDashboard2.classList.add("text-primary");
+    btnDashboard1.classList.remove("btn-primary");
+    btnDashboard1.classList.remove("text-white");
+    btnDashboard1.classList.add("btn-outline-light");
+    btnDashboard1.classList.add("text-primary");
+
     btnDashboard2.href = "../pages/account.html";
     btnDashboard1.href = "../pages/account.html";
     tittlePage.textContent = "Unauthenticans Page";
   } else if (currentAccount().privilege === "admin") {
+    logoutBtn1.classList.remove("d-none");
+    logoutBtn2.classList.remove("d-none");
     container.innerHTML = adminDashboard(JSON.stringify(accountData, 0));
     btnDashboard2.innerText = currentAccount().privilege;
     btnDashboard1.innerText = currentAccount().privilege;
@@ -86,6 +99,8 @@ const currentAccount = () => {
     btnDashboard1.href = "#";
     tittlePage.textContent = "Admin Dashboard Page";
   } else if (currentAccount().privilege === "user") {
+    logoutBtn1.classList.remove("d-none");
+    logoutBtn2.classList.remove("d-none");
     container.innerHTML = userDashboard();
     btnDashboard2.innerText = `${currentAccount().privilege} ${
       currentAccount().id
@@ -98,6 +113,33 @@ const currentAccount = () => {
     tittlePage.textContent = "User Dashboard Page";
   }
 })();
+
+logoutBtn1.addEventListener("click", () => {
+  localStorage.removeItem("tempSessionAccount");
+
+  // re-direct
+  const regexLocalhost = /localhost/i;
+  const currentUrl = window.location.href;
+  if (regexLocalhost.test(currentUrl)) {
+    window.location.href = "http://localhost:5501/pages/account.html";
+  } else {
+    window.location.href =
+      "https://k3ndev.github.io/web-pulse/pages/account.html";
+  }
+});
+logoutBtn2.addEventListener("click", () => {
+  localStorage.removeItem("tempSessionAccount");
+
+  // re-direct
+  const regexLocalhost = /localhost/i;
+  const currentUrl = window.location.href;
+  if (regexLocalhost.test(currentUrl)) {
+    window.location.href = "http://localhost:5501/pages/account.html";
+  } else {
+    window.location.href =
+      "https://k3ndev.github.io/web-pulse/pages/account.html";
+  }
+});
 
 // PROBLEM
 // when you open 2 tab, and login at the same with different credential ... they have their own different session
--- js/home_firstRender.js
+++ js/home_firstRender.js
@@ -3,6 +3,8 @@
 // dom
 const btnDashboard1 = document.querySelector("#btn-dashboard1");
 const btnDashboard2 = document.querySelector("#btn-dashboard2");
+const logoutBtn1 = document.querySelector("#logoutBtn1");
+const logoutBtn2 = document.querySelector("#logoutBtn2");
 
 // temp storage
 const accountData = JSON.parse(localStorage.getItem("tempAccountData"));
@@ -22,21 +24,23 @@ const currentAccount = () => {
 (() => {
   if (sessionAccount !== null) {
     if (currentAccount().privilege === "admin") {
+      logoutBtn1.classList.remove("d-none");
+      logoutBtn2.classList.remove("d-none");
       btnDashboard2.innerText = currentAccount().privilege;
       btnDashboard1.innerText = currentAccount().privilege;
       const regexLocalhost = /localhost/i;
       const currentUrl = window.location.href;
       if (regexLocalhost.test(currentUrl)) {
-        console.log("if 1st");
         btnDashboard2.href = "./pages/dashboard.html";
         btnDashboard1.href = "./pages/dashboard.html";
       } else {
-        console.log("if 2nd");
         btnDashboard2.href = "/web-pulse/pages/dashboard.html";
         btnDashboard1.href = "/web-pulse/pages/dashboard.html";
       }
     }
     if (currentAccount().privilege === "user") {
+      logoutBtn1.classList.remove("d-none");
+      logoutBtn2.classList.remove("d-none");
       btnDashboard2.innerText = `${currentAccount().privilege} ${
         currentAccount().id
       }`;
@@ -54,5 +58,40 @@ const currentAccount = () => {
         btnDashboard1.href = "/web-pulse/pages/dashboard.html";
       }
     }
+  } else {
+    console.log("hi");
+    btnDashboard2.classList.remove("btn-primary");
+    btnDashboard2.classList.remove("text-white");
+    btnDashboard2.classList.add("btn-outline-light");
+    btnDashboard2.classList.add("text-primary");
+    btnDashboard1.classList.remove("btn-primary");
+    btnDashboard1.classList.remove("text-white");
+    btnDashboard1.classList.add("btn-outline-light");
+    btnDashboard1.classList.add("text-primary");
   }
 })();
+
+logoutBtn1.addEventListener("click", () => {
+  localStorage.removeItem("tempSessionAccount");
+
+  // re-direct
+  const regexLocalhost = /localhost/i;
+  const currentUrl = window.location.href;
+  if (regexLocalhost.test(currentUrl)) {
+    window.location.href = "http://localhost:5501/index.html";
+  } else {
+    window.location.href = "https://k3ndev.github.io/web-pulse/";
+  }
+});
+logoutBtn2.addEventListener("click", () => {
+  localStorage.removeItem("tempSessionAccount");
+
+  // re-direct
+  const regexLocalhost = /localhost/i;
+  const currentUrl = window.location.href;
+  if (regexLocalhost.test(currentUrl)) {
+    window.location.href = "http://localhost:5501/index.html";
+  } else {
+    window.location.href = "https://k3ndev.github.io/web-pulse/";
+  }
+});
